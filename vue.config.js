fe0f124212ce0db4d7855eae908b66bd30ffc39b
ozanuslan <ozanuslan@hotmail.com>
2021-07-11 13:32:48 +0300

    dev_host_check false

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,9 @@
 module.exports = {
-  configureWebpack: {
-    output: {
-      publicPath: '/static/',
-    },
-  },
   devServer: {
     proxy: {
       '/api': {
         target: 'https://direcon-task.herokuapp.com',
+        disableHostCheck: true,
       },
     },
   },
