2847f729baf4b2e97e581582b5eae48a652c6b75
ozanuslan <ozanuslan@hotmail.com>
2021-07-11 13:38:11 +0300

    .

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,7 @@ module.exports = {
       '/api': {
         target: 'https://direcon-task.herokuapp.com',
         disableHostCheck: true,
+        allowedHosts: ['.herokuapp.com', 'herokuapp.com', 'localhost'],
       },
     },
   },
